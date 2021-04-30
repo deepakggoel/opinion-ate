728cdc0cdeac702755f57d83fbc565874f1f3f6c
Deepak Goel <deepak@agaminsoft.com>
2021-04-29 17:33:35 -0700

    Set up linting and autoformatting, added vscode launch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Greet() {
+  return 'Hello, world!';
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload it - {Greet()}.
         </p>
         <a
           className="App-link"
